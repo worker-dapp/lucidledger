4368342956ec93395ff190294c89753f3e69e932
hanumantjain <hanumantjain939@gmail.com>
2025-10-07 13:40:08 -0400

    jobs and landing page updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="py-20 text-center">
+    <div className="py-8 text-center">
       © 2025 Lucid Ledger. All rights reserved.
     </div>
   );
