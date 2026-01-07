e47d377620e638341d731e0e41c06d5ae7ea1e98
Emmanuel Teitelbaum <emmanuel.teitelbaum@gmail.com>
2026-01-06 23:57:50 -0500

    update copyright year in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="py-8 text-center">
-      © 2025 Lucid Ledger. All rights reserved.
+      © 2026 Lucid Ledger. All rights reserved.
     </div>
   );
 };
